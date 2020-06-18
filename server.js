bbb5445cc5a9d2e62e6d3b453a1ee9dc8ace496e
Cattleya Pia <cjpia612@gmail.com>
2020-06-17 22:47:00 -0700

    notes db.json get possible

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,17 +8,31 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+// ROUTES
 
-// GET
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
+app.get("/api/notes", (req, res) => {
+    fs.readFile(__dirname + "public/notes.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html"});
+        res.json(__dirname, "db/db.json");
+    });
+});
 
-
-
+// app.post("/api/notes", (req, res) => {
+//     const notes = req.body;
+  
+//     notes.routeName = waitingTable.name.replace(/\s+/g, "").toLowerCase();
+  
+//     waitingList.push(waitingTable);
+  
+//     res.json(waitingTable);
+// });
 
 
 
