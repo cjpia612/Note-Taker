17558fa0c33ac1ef057590228671003924a1f85d
Cattleya Pia <cjpia612@gmail.com>
2020-06-17 22:01:46 -0700

    first get path working properly

--- server.js
+++ server.js
@@ -4,3 +4,26 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+// GET
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
